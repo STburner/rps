d868420814b9916df4fa28d0f568c6ce22e26eae
Armaan <143736190+Airman0@users.noreply.github.com>
2024-03-20 13:37:13 -0400

    Scoreboard

--- rps_hard.py
+++ rps_hard.py
@@ -1,7 +1,12 @@
 import random
+
 def rock_paper_scissors():
-    print("Hello Mr. Teacher, welcome to the extremly convoluted and complicated amazing, super game of Rock, Paper, Scissors!")
-    print("I am not sorry for your inconvienince")
+    print("Hello Mr. Teacher, welcome to the extremely convoluted and complicated amazing, super game of Rock, Paper, Scissors!")
+    print("I am not sorry for your inconvenience")
+    
+    player_wins = 0
+    computer_wins = 0
+    
     while True:
         print("\nChoose your attack:")
         print("1. Rock")
@@ -11,11 +16,12 @@ def rock_paper_scissors():
         ur_move = input("Enter your choice, 1, 2, or 3: ")
         if ur_move not in ['1', '2', '3']:
             print("Come on, enter something valid, aka, 1, 2, 3")
+            continue
 
         ur_move = int(ur_move)
         computer_move = random.randint(1, 3)
         
-        print("Ur move:", end=" ")
+        print("Your move:", end=" ")
         if ur_move == 1:
             print("Rock")
         elif ur_move == 2:
@@ -37,11 +43,16 @@ def rock_paper_scissors():
              (ur_move == 2 and computer_move == 1) or \
              (ur_move == 3 and computer_move == 2):
             print("You win, wowowowow")
+            player_wins += 1
         else:
             print("Computer wins, take the L")
-        play_again = input("Do you want to play again, yes or no? (please say no)")
-        if (play_again.lower()!="yes"):
-            print("You're done, get reckd")
+            computer_wins += 1
+        
+        print("Scoreboard - You:", player_wins, "Computer:", computer_wins)
+        
+        play_again = input("You wanna waste your time on another round? (yes or no): ")
+        if play_again.lower() != "yes":
+            print("You're done, get wrecked")
             break
 
 rock_paper_scissors()
