c7d5feebe3442b86df515b6868dc68bc222e234a
Shantanu Tiwari <143736598+STburner@users.noreply.github.com>
2024-03-26 13:04:02 -0400

    Update rps_hard.py

--- rps_hard.py
+++ rps_hard.py
@@ -1,57 +1,81 @@
 import random
 
-def rock_paper_scissors():
+def welcome_mr_schultze():
     print("Hello Mr. Teacher, welcome to the extremely convoluted and complicated amazing, super game of Rock, Paper, Scissors!")
     print("I am not sorry for your inconvenience")
-    
-    player_wins = 0
-    computer_wins = 0
-    
+
+def get_player_choice():
     while True:
         print("\nChoose your attack:")
         print("1. Rock")
         print("2. Paper")
         print("3. Scissors")
 
-        ur_move = input("Enter your choice, 1, 2, or 3: ")
-        if ur_move not in ['1', '2', '3']:
+        try:
+            ur_move = int(input("Enter your choice, 1, 2, or 3: "))
+            if ur_move not in [1, 2, 3]:
+                raise ValueError
+            return ur_move
+        except ValueError:
             print("Come on, enter something valid, aka, 1, 2, 3")
-            continue
 
-        ur_move = int(ur_move)
-        computer_move = random.randint(1, 3)
-        
-        print("Your move:", end=" ")
-        if ur_move == 1:
-            print("Rock")
-        elif ur_move == 2:
-            print("Paper")
+def get_computer_choice():
+    return random.randint(1, 3)
+
+def print_moves(player_move, computer_move):
+    moves = {1: "Rock", 2: "Paper", 3: "Scissors"}
+    print("Your move:", moves[player_move])
+    print("Computer move:", moves[computer_move])
+
+def get_winner(player_move, computer_move):
+    if player_move == computer_move:
+        return "tie"
+    elif (player_move == 1 and computer_move == 3) or \
+         (player_move == 2 and computer_move == 1) or \
+         (player_move == 3 and computer_move == 2):
+        return "player"
+    else:
+        return "computer"
+
+def print_result(winner):
+    if winner == "tie":
+        print("It's a tie!!! Now you have to waste your time on another round.")
+    elif winner == "player":
+        print("You win, wowowowow")
+    else:
+        print("Computer wins, take the L")
+
+
+def play_again():
+    while True:
+        choice = input("You wanna waste your time on another round? (yes or no): ").strip().lower()
+        if choice == "yes":
+            return True
+        elif choice == "no":
+            return False
         else:
-            print("Scissors")
+            print("Please enter 'yes' or 'no'.")
+
+def rock_paper_scissors():
+    welcome_mr_schultze()
+    
+    player_wins = 0
+    computer_wins = 0
+    
+    while True:
+        player_choice = get_player_choice()
+        computer_choice = get_computer_choice()
+        print_moves(player_choice, computer_choice)
+        winner = get_winner(player_choice, computer_choice)
+        print_result(winner)
         
-        print("Computer move:", end=" ")
-        if computer_move == 1:
-            print("Rock")
-        elif computer_move == 2:
-            print("Paper")
-        else:
-            print("Scissors")
-        
-        if ur_move == computer_move:
-            print("It's a tie!!! Now you have to waste your time on another round.")
-        elif (ur_move == 1 and computer_move == 3) or \
-             (ur_move == 2 and computer_move == 1) or \
-             (ur_move == 3 and computer_move == 2):
-            print("You win, wowowowow")
+        if winner == "player":
             player_wins += 1
-        else:
-            print("Computer wins, take the L")
+        elif winner == "computer":
             computer_wins += 1
-        
         print("Scoreboard - You:", player_wins, "Computer:", computer_wins)
         
-        play_again = input("You wanna waste your time on another round? (yes or no): ")
-        if play_again.lower() != "yes":
+        if not play_again():
             print("You're done, get wrecked")
             break
 
